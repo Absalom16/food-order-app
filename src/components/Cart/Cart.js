2b426ad5db6b11bb4dfc4ff303116c89ec9e6cae
Absalom16 <absalomlihasi@gmail.com>
2023-06-11 17:26:04 +0300

    cart implementation

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+
+const Cart = (props) => {
+  const CartCtx = useContext(CartContext);
+
+  const totalAmount = `$${CartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = CartCtx.items.length > 0;
+
+  const cartItems = (
+    <ul>
+      {CartCtx.items.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
